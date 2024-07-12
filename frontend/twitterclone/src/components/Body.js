570830384be74ce0b50c5b361ca6888b04cd2501
Pran2535 <126868179+Pran2535@users.noreply.github.com>
2024-07-12 09:48:43 +0530

    first commit

--- frontend/twitterclone/src/components/Body.js
+++ frontend/twitterclone/src/components/Body.js
@@ -1,36 +1,36 @@
-import React from 'react';
+import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Login from './Login';
-import Home from './Home';
-import Feed from './Feed';
-import Profile from './Profile';
+import Login from "./Login";
+import Home from "./Home";
+import Feed from "./Feed";
+import Profile from "./Profile";
 
 const Body = () => {
-    const appRouter = createBrowserRouter([
+  const appRouter = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+      children: [
         {
-            path: "/",
-            element: <Home/>,
-            children:[
-                {
-                    path:"/",
-                    element:<Feed/>
-                },
-                {
-                    path:"/profile/:id",
-                    element:<Profile/>
-                }
-            ]
+          path: "/",
+          element: <Feed />,
         },
         {
-            path: "/login",
-            element: <Login />
-        }
-    ])
-    return (
-        <div>
-            <RouterProvider router={appRouter} />
-        </div>
-    )
-}
+          path: "/profile/:id",
+          element: <Profile />,
+        },
+      ],
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+  ]);
+  return (
+    <div>
+      <RouterProvider router={appRouter} />
+    </div>
+  );
+};
 
-export default Body
+export default Body;
